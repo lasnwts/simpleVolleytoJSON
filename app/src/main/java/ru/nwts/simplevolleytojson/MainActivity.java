3e190fcb272750e67697cf9cfb69000862bc5883
Alexander Lyapustin <lasnwts@gmail.com>
2016-11-09 11:52:41 +0300

    Initial commit

---
+++ app/src/main/java/ru/nwts/simplevolleytojson/MainActivity.java
@@ -0,0 +1,19 @@
+package ru.nwts.simplevolleytojson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * https://www.simplifiedcoding.net/android-volley-tutorial-to-get-json-from-server/
+     * Android Volley Tutorial to Get JSON from Server
+     * @param savedInstanceState
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
