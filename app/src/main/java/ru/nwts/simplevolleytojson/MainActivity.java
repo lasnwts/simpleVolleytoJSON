469a0d8fb705c913d1e48dd6d7677348888341f6
lasnwts <lasnwts@gmail.com>
2016-11-09 12:11:08 +0300

    Update MainActivity.java
    I testets

--- app/src/main/java/ru/nwts/simplevolleytojson/MainActivity.java
+++ app/src/main/java/ru/nwts/simplevolleytojson/MainActivity.java
@@ -15,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void test(){
+      int i = 1;
     }
 }
